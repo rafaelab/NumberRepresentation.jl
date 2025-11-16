4b41bc670228bd260dcb3bcfc8329c949657ee8e
Rafael <rafaelab@users.noreply.github.com>
2025-11-16 20:17:48 +0100

    fix bugs in tests

--- test/makieRT.jl
+++ test/makieRT.jl
@@ -1,5 +1,17 @@
 @testset "NumberRepresentationMakieRichTextExt" begin
 
+	@testset "constructor" begin
+		x = 1234.5678
+		config1 = NumberRepresentationConfig(; decimals = 3)
+		reprU1 = NumberRepresentationUnicode(x, FixedPointNotation, config1)
+		reprR1 = NumberRepresentationMakieRichText(reprU1)
+		reprR2 = NumberRepresentationMakieRichText(x, FixedPointNotation, config1)
+		@test reprR1.number == x
+		@test reprR2.number == x
+		@test reprR1.timesSymbol == reprU1.timesSymbol
+		@test typeof(reprR1.representation) == typeof(Makie.rich(reprU1.representation))		
+	end
+
 	@testset "notations" begin
 		reprU = NumberRepresentationUnicode(3.14159, FixedPointNotation; decimals = 2)
 		reprR = NumberRepresentationMakieRichText(reprU)
