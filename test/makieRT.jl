27bfe6a70edcb951f8214d4f2b36ad79c41979c3
Rafael <rafaelab@users.noreply.github.com>
2025-11-16 19:47:11 +0100

    remove misplaced test

--- test/makieRT.jl
+++ test/makieRT.jl
@@ -1,14 +1,5 @@
 @testset "NumberRepresentationMakieRichTextExt" begin
 
-	@testset "basic behaviour" begin
-		repr = NumberRepresentationPlain(12.34, FixedPointNotation)
-		repr2 = NumberRepresentationUnicode(12.34, ScientificNotation)
-		repr3 = NumberRepresentationTeX(12.34, EngineeringNotation)
-		@test typeof(repr1.representation) == String
-		@test typeof(repr2.representation) == String
-		@test typeof(repr3.representation) == String
-	end
-
 	@testset "notations" begin
 		reprU = NumberRepresentationUnicode(3.14159, FixedPointNotation; decimals = 2)
 		reprR = NumberRepresentationMakieRichText(reprU)
