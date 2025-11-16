e5bb64bac68c97bff22f77cc6c8694830e0e275c
Rafael <rafaelab@users.noreply.github.com>
2025-11-16 18:43:22 +0100

    rename tolerance field

---
+++ src/config.jl
@@ -0,0 +1,68 @@
+# ----------------------------------------------------------------------------------------------- #
+#
+@doc """
+	NumberRepresentationConfig{E <: Real, I <: Integer}
+
+Configuration struct for number representations.
+
+# Fields
+. `signSignificand` [`Bool`]: whether to print the significand's sign \\
+. `signExponent` [`Bool`]: whether to print the exponent's sign \\
+. `shortenOneTimes` [`Bool`]: whether to write numbers of the form 1xB^E as B^E \\
+. `shortenBaseToZero` [`Bool`]: whether to write numbers like B^0 as 1 \\
+. `decimals` [`Integer`]: number of decimals of the significand \\
+. `toleranceShort` [`Real`]: tolerance for comparisons when shortening (absolute) \\
+"""
+struct NumberRepresentationConfig{E  <: Real, I <: Integer}
+	signSignificand::Bool
+	signExponent::Bool
+	shortenOneTimes::Bool
+	shortenBaseToZero::Bool
+	decimals::I
+	toleranceShort::E
+end
+
+NumberRepresentationConfig(; signSignificand::Bool = false, signExponent::Bool = false, shortenOneTimes::Bool = false, shortenBaseToZero::Bool = false, decimals::Integer = 6, toleranceShort::Real = 1e-8, args...) = begin
+	if ! isempty(args)
+		unknown = join(keys(args), ", ")
+		@warn("Unknown keyword(s) for NumberRepresentationConfig: $(unknown). Ignoring it (them).")
+	end
+
+	return NumberRepresentationConfig{typeof(toleranceShort), typeof(decimals)}(signSignificand, signExponent, shortenOneTimes, shortenBaseToZero, decimals, toleranceShort)
+end
+
+NumberRepresentationConfig(d::Dict{Symbol, Any}) = begin
+	return NumberRepresentationConfig(; d...)
+end
+
+NumberRepresentationConfig(d::NamedTuple) = begin
+	return NumberRepresentationConfig(; d...)
+end
+
+
+# ----------------------------------------------------------------------------------------------- #
+#
+@doc """
+	toDict(cfg::NumberRepresentationConfig)
+
+Convert a `NumberRepresentationConfig` object to a dictionary.
+
+# Input
+. `cfg` [`NumberRepresentationConfig`]: the configuration object \\
+
+# Output
+. A `Dict{Symbol, Any}` with the configuration parameters.
+"""
+function toDict(cfg::NumberRepresentationConfig)
+	return Dict(
+		:signSignificand => cfg.signSignificand,
+		:signExponent => cfg.signExponent,
+		:shortenOneTimes => cfg.shortenOneTimes,
+		:shortenBaseToZero => cfg.shortenBaseToZero,
+		:decimals => cfg.decimals,
+		:toleranceShort => cfg.toleranceShort
+	)
+end
+
+
+# ----------------------------------------------------------------------------------------------- #
